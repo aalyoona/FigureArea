17462f390d5d930e8f2f24957db61d71791cb1f2
Alyona <aalyoona28@gmail.com>
2023-04-21 13:13:40 +0300

    Add sql script for test task

---
+++ GetAllProductsWithAndWithoutCategories.sql
@@ -0,0 +1,4 @@
+SELECT Products.Name AS ProductName, Categories.Name AS CategoryName
+FROM Products
+LEFT JOIN ProductCategories ON Products.Id = ProductCategories.ProductId
+LEFT JOIN Categories ON Categories.Id = ProductCategories.CategoryId
